29bade8bcaee5099c98bbdfde8059c0423910acb
rfqu <rfq@list.ru>
2015-06-06 00:19:21 +0600

    junit mix fixed

--- df4j-core/example/com/github/rfqu/df4j/gparsDemo/DataflowOperatorTest.java
+++ df4j-core/example/com/github/rfqu/df4j/gparsDemo/DataflowOperatorTest.java
@@ -1,7 +1,7 @@
 package com.github.rfqu.df4j.gparsDemo;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.ArrayBlockingQueue;
 
